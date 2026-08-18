4258f8938cb0c2d68726b19ce12960554fe93eaa
Nicholas <Nickmilanin@gmail.com>
2026-08-18 14:40:18 -0300

    h2, database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,6 +3,14 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	/**
 		No java existe uma interface chamada Serializable que
@@ -11,10 +19,23 @@ public class User implements Serializable {
 		
 		Isso serve para permitir que os objetos trafegue na rede,
 		para que seja gravado em arquivos, etc.
+		
+		As anotações dizem ao Spring que os objetos dessa classe
+		são entidades e define o nome que a tabela de usuários
+		deve ter no banco de dados.
+		
+		É importante ressaltar que nós chamamos as interfaces
+		do JPA e não a implementação (hibernate) pois é melhor
+		dependendo da interface do que da implementação, assim
+		caso o projeto mude para outra implementação do JPA ele
+		continuará funcionando.
 	*/
 	
 	
 	private static final long serialVersionUID = 1L;
+	
+	@Id					// Essa anotação diz ao Spring que a variável "id" é o identificador da entidade dentro do banco de dados.
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	// Essa anotação serve para gerar o ID do objeto de forma auto-incremental.
 	private Long id;
 	private String name;
 	private String email;
