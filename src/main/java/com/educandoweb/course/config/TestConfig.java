696eef19a91d48c6ece79295350c0c8b4fd95628
Nicholas <Nickmilanin@gmail.com>
2026-08-19 11:09:12 -0300

    Category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
@@ -38,10 +40,16 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
-		// Criando os usuários que vão ser salvos no banco de dados
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "97777777", "123456");
 		
@@ -58,6 +66,7 @@ public class TestConfig implements CommandLineRunner {
 			comandos SQL diretamente, ao invés de utilizar JDBC e escrever
 			os comandos na mão, basta utilizar esse comando do JPA.
 		*/
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
---
+++ src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.course.entities.Category;
+
+// @ Repository - Poderiamos colocar essa anotação para o Spring entender que essa classe é da camada de repository.
+// Porém como essa classe é filha da JpaRepository o Spring já identifica ela como Repository, então essa
+// anotação é opcional
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	/**
+		Esta é a camada de data Repository, que se comunica com o banco de dados para recuperar
+		informações.
+		
+		Nós não precisamos criar uma implementação dessa interface, pois o SpringJPA 
+		já possui uma implementação para ela.
+	*/
+	
+	
+}
