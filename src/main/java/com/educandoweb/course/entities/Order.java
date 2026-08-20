9571d85c858cfb8491f3da50c5a93ee7b86e65b9
Nicholas <Nickmilanin@gmail.com>
2026-08-20 14:02:43 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -42,6 +44,13 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")					// Utilizamos "id.order" pois os product estão dentro do atributo ID no objeto "OrderItem"
 	private Set<OrderItem> items = new HashSet<>();
+	
+	/*
+		No caso do 1 para 1, nós estamos mapeando as duas entidades para ter o mesmo ID
+		Essa relação 1 para 1 é obrigatório adicionar o CascadeType.ALL.
+	*/
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	// Construtores
 	public Order() {}
@@ -109,8 +118,14 @@ public class Order implements Serializable {
 	}
 	public void setItems(Set<OrderItem> items) {
 		this.items = items;
+	}
+	
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}	
-
-	
-	
 }
