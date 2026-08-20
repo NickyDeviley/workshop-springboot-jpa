65d3985af72ff7fcae391125e0cbd1c74be3a08b
Nicholas <Nickmilanin@gmail.com>
2026-08-20 16:51:47 -0300

    User Insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -35,4 +35,10 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		
+		return repository.save(obj);
+		
+	}
 }
